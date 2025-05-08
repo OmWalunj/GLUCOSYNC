2d7f3a018c3204e79fa763e926c52c939f3f7640
tanmaymay27 <111596942+tanmaymay27@users.noreply.github.com>
2025-05-08 17:31:04 +0530

    first commit

---
+++ glucosync/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+// import 'package:glucosync/pages/login.dart';
+import 'package:glucosync/pages/splash_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'Enter ur Supabase URL',
+    anonKey: 'Enter ur Supabase Anon Key',
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GlucoSync',
+      home: Splashscreen(),
+    );
+  }
+}
